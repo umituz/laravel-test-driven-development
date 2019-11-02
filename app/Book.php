6fd8c29ba7eea9d5fb562dc84a014f295992a70a
Ümit UZ <umituz998@gmail.com>
2019-11-02 20:28:28 +0300

    BookReservationsTest was written for unit test

--- app/Book.php
+++ app/Book.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use mysql_xdevapi\Exception;
 
 /**
  * Class Book
@@ -25,6 +26,36 @@ class Book extends Model
         return '/books/' . $this->id;
     }
 
+    public function checkout(User $user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    /**
+     * @param User $user
+     * @throws \Exception
+     */
+    public function checkin(User $user)
+    {
+        $reservation = $this->reservations()
+            ->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if(is_null($reservation)){
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now()
+        ]);
+
+    }
+
     /**
      * @param $author
      */
@@ -33,5 +64,10 @@ class Book extends Model
         $this->attributes['author_id'] = (Author::firstOrCreate([
             'name' => $author
         ]))->id;
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
