6298055ff3408308b20310801867d68207f63aa8
Ümit UZ <umituz998@gmail.com>
2021-05-27 18:03:22 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function(){
+    return view('welcome');
+});
+
 Route::post('/books', 'BooksController@store');
 Route::patch('/books/{book}', 'BooksController@update');
 Route::delete('/books/{book}', 'BooksController@destroy');
